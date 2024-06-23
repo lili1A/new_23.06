dc99acdc8483c77ba58d54f546e347e9b10f6441
lili1A <154353393+lili1A@users.noreply.github.com>
2024-06-23 14:35:20 +0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+total_rm = 520 
+days = 17-4
+print(days)
+spend_rm = total_rm / days
+print(spend_rm, "rm")
+total_usd = 464 
+airpods = total_usd -234 
+print(airpods, "usd")
+spend_usd = airpods / 13
+print(spend_usd, " usd")
+
+
+
